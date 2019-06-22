d0e13fbe7f038d098c7cc92bf7f14d3c7160183a
dimasm93 <software.dimas_m@icloud.com>
2019-06-23 00:09:31 +0700

    pembahasan tipe data number -> byte

---
+++ src/main/java/com/maryanto/dimas/bootcamp/variables/TipeDataNumber.java
@@ -0,0 +1,11 @@
+package com.maryanto.dimas.bootcamp.variables;
+
+public class TipeDataNumber {
+
+    public static void main(String[] args) {
+        Byte min = -128;
+        Byte max = 10;
+
+        System.out.println("byte nilai minimumnya adalah "+ min +" dan maximumnya adalah "+ max);
+    }
+}
