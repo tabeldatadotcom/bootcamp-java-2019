9a6b16522bc449381767c5b360f159e0bf421d65
dimasm93 <software.dimas_m@icloud.com>
2019-06-23 00:28:06 +0700

    tipe data number -> long

--- src/main/java/com/maryanto/dimas/bootcamp/variables/TipeDataNumber.java
+++ src/main/java/com/maryanto/dimas/bootcamp/variables/TipeDataNumber.java
@@ -3,8 +3,8 @@ package com.maryanto.dimas.bootcamp.variables;
 public class TipeDataNumber {
 
     public static void main(String[] args) {
-        Integer min = -2_147_483_648;
-        Integer max = 2_147_483_647;
+        Long nilaiMin = -9_223_372_036_854_775_808l;
+        Long nilaiMax = 9_223_372_036_854_775_807l;
 
         System.out.println("byte nilai minimumnya adalah "+ min +" dan maximumnya adalah "+ max);
     }
