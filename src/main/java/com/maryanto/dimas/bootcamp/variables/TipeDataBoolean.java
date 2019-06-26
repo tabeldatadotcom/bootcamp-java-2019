07c3aa539be049b86fa204095fbf01bc95e0252e
dimasm93 <software.dimas_m@icloud.com>
2019-06-27 00:07:58 +0700

    tipe data boolean

---
+++ src/main/java/com/maryanto/dimas/bootcamp/variables/TipeDataBoolean.java
@@ -0,0 +1,10 @@
+package com.maryanto.dimas.bootcamp.variables;
+
+public class TipeDataBoolean{
+    
+    public static void main(String[] args){
+        Boolean isAuthenticate = true;
+
+        System.out.println("Apakah anda telah login? "+ (isAuthenticate ? "Sudah" : "Belum"));
+    }
+}
