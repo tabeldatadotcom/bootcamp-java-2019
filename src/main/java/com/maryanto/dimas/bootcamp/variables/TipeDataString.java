cc98efec8b6906772de63ae77842f6bfbed854d0
dimasm93 <software.dimas_m@icloud.com>
2019-06-27 22:56:03 +0700

    tipe data char

---
+++ src/main/java/com/maryanto/dimas/bootcamp/variables/TipeDataString.java
@@ -0,0 +1,10 @@
+package com.maryanto.dimas.bootcamp.variables;
+
+public class TipeDataString{
+
+    public static void main(String[] args){
+
+        char sebuahKarakter = 'D';
+        System.out.println("Ini adalah sebuah karakter :"+ sebuahKarakter);
+    }
+}
