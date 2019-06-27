c8fc1bad61709903983b09a8aab8f7afef653b51
dimasm93 <software.dimas_m@icloud.com>
2019-06-27 23:03:20 +0700

    tipe data string

--- src/main/java/com/maryanto/dimas/bootcamp/variables/TipeDataString.java
+++ src/main/java/com/maryanto/dimas/bootcamp/variables/TipeDataString.java
@@ -4,7 +4,10 @@ public class TipeDataString{
 
     public static void main(String[] args){
 
-        char sebuahKarakter = 'D';
-        System.out.println("Ini adalah sebuah karakter :"+ sebuahKarakter);
+        // char sebuahKarakter = 'D';
+        // System.out.println("Ini adalah sebuah karakter :"+ sebuahKarakter);
+
+        String sebuahString = "Dimas Maryanto";
+        System.out.println("ini adalah nilai dari sebuah String: "+ sebuahString);
     }
 }
