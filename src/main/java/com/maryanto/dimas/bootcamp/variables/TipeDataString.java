8b4f4059bf592cd9c1b33dfbf568bbebd002276b
dimasm93 <software.dimas_m@icloud.com>
2019-06-27 23:14:13 +0700

    penjelasan kelas String

--- src/main/java/com/maryanto/dimas/bootcamp/variables/TipeDataString.java
+++ src/main/java/com/maryanto/dimas/bootcamp/variables/TipeDataString.java
@@ -8,6 +8,11 @@ public class TipeDataString{
         // System.out.println("Ini adalah sebuah karakter :"+ sebuahKarakter);
 
         String sebuahString = "Dimas Maryanto";
-        System.out.println("ini adalah nilai dari sebuah String: "+ sebuahString);
+        System.out.println("ini adalah nilai dari sebuah String: "+ sebuahString.toUpperCase());
+        System.out.println("");
+        System.out.println("ini adalah nilai dari sebuah String pada index ke 3: "+ sebuahString.charAt(3));
+        System.out.println("");
+        System.out.println("apakah nilai " + (sebuahString.equalsIgnoreCase("dimas maryanto") ? "Sama" : "Berbeda!"));
+
     }
 }
