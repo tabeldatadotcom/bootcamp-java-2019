d042e5f206f1b6584e5479a035c38656c57aa1ae
dimasm93 <software.dimas_m@icloud.com>
2019-07-08 00:30:56 +0700

    tipe data date dengan local date and time

--- src/main/java/com/maryanto/dimas/bootcamp/variables/TipeDataDate.java
+++ src/main/java/com/maryanto/dimas/bootcamp/variables/TipeDataDate.java
@@ -1,12 +1,27 @@
 package com.maryanto.dimas.bootcamp.variables;
 
-import java.util.Date;
-import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+// import java.util.Date;
+// import java.text.SimpleDateFormat;
 
 public class TipeDataDate{
     public static void main(String[] args) {
-        Date tanggalSekarang = new Date();
-        SimpleDateFormat dateFormater = new SimpleDateFormat("dd MMM yyyy'T'HH:mm:ss");
-        System.out.println(String.format("sekarang tanggal : %s", dateFormater.format(tanggalSekarang)));
+        // Date tanggalSekarang = new Date();
+        // SimpleDateFormat dateFormater = new SimpleDateFormat("dd MMM yyyy'T'HH:mm:ss");
+        
+        LocalDate tanggalSekarang = LocalDate.now();
+        System.out.println(String.format("sekarang tanggal : %s", tanggalSekarang));
+
+        LocalDateTime tanggalDanWaktuSekarang = LocalDateTime.now();
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd-MMM-yyyy'T'HH:mm:ss");
+        System.out.println(
+            String.format("sekarang tanggal dan waktu : %s", dateFormat.format(tanggalDanWaktuSekarang)));
+
+        LocalTime waktuSekarang = LocalTime.now();
+        System.out.println(String.format("sekarang waktu : %s", waktuSekarang));
     }
 }
